8d328bdbef1570cc324987ce7c3359b8b030c751
garrettomi <omigarrett@gmail.com>
2023-05-31 09:39:29 +0900

    Feat: Got all the pokemon

--- myapp/api.py
+++ myapp/api.py
@@ -1,26 +1,48 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polyglottalbackend.settings')
+
+import django
+django.setup()
+
 import requests
 import json
+from myapp.models import Pokemon
 
-response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=10')
-parsed_data = json.loads(response.text)
-print(parsed_data)
-all_pokemon = parsed_data['results']
+def fetch_pokemon(page=1, limit=20):
+    print(f"Fetching page {page}")
+    offset = (page - 1) * limit
+    url = f'https://pokeapi.co/api/v2/pokemon?offset={offset}&limit={limit}'
+    response = requests.get(url)
 
+    if response.status_code == 200:
 
+        parsed_data = json.loads(response.text)
+        all_pokemon = parsed_data['results']
 
-for pokemon_data in all_pokemon:
-    print(pokemon_data['name'])
-    pokemon_url = pokemon_data['url']
-    pokemon_response = requests.get(pokemon_url)
-    pokemon_details = json.loads(pokemon_response.text)
-    print(pokemon_details['sprites']['front_default'])
+        for pokemon_data in all_pokemon:
+            name = pokemon_data['name']
+            print(f"Fetching {name}")
+            pokemon_url = pokemon_data['url']
+            pokemon_response = requests.get(pokemon_url)
+            pokemon_details = json.loads(pokemon_response.text)
+            img_url = pokemon_details['sprites']['front_default']
+            Pokemon.objects.get_or_create(name=name, defaults={'img_url':img_url})
+            
+        return parsed_data['next'] is not None
+    else:
+        print("An error occured fetching Pokemon")
+        return False
 
-# all_pokemon = json.loads(response.text)
+def fetch_all_pokemon():
+       page = 1
+       limit = 20
+       has_more = True
 
-# for pokemon_data in all_pokemon:
-    # name = pokemon_data['name']
-    # img_url = pokemon_data['sprites']['front_default']
-
-
-# for pokemon_data in all_pokemon:
-#     print(pokemon_data)
+       while has_more:
+            has_more = fetch_pokemon(page, limit)
+            page += 1
+    
+fetch_all_pokemon()
--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Pokemon(models.Model):
-    name = models.CharField(max_length=255)
-    img_url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+    img_url = models.URLField(null=True)
+
+    class Meta:
+        app_label = 'myapp'
 
     def __str__(self):
         return self.name
